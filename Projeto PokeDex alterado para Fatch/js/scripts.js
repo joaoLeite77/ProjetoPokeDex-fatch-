bb67df7aac987014d0a09e88b8f46654a57cc35f
joaoLeite77 <129081324+joaoLeite77@users.noreply.github.com>
2024-05-07 20:38:38 -0300

    Add files via upload

---
+++ Projeto PokeDex alterado para Fatch/js/scripts.js
@@ -0,0 +1,50 @@
+
+
+const grupoGrama = ["bulbasaur","venusaur","ivysaur"]
+const grupoFogo = ["charmander","charmeleon","charizard"]
+
+
+function carregarPokemons() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
+      .then(response => response.json())
+      .then(data => {
+        const pokemons = data.results;
+  
+        for (let index = 0; index < pokemons.length; index++) {
+          const pokemon = pokemons[index];
+          const id = index + 1;
+          pokemon.imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+          adicionarCardPokemon(pokemon);
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
+    loadComponents()
+
+function adicionarCardPokemon(pokemon){
+
+    const divCardElemento = document.createElement("div")
+    divCardElemento.classList.add("card")
+
+    const imagemElemento = document.createElement("img")
+    imagemElemento.setAttribute("src", pokemon.imagem)
+    imagemElemento.style.width="90%"
+
+    const textoElemento = document.createElement("h1")
+    textoElemento.innerHTML = pokemon.name
+
+    if(grupoGrama.includes(pokemon.name)){
+        textoElemento.classList.add("grama")
+    } else if(grupoFogo.includes(pokemon.name)){
+        textoElemento.classList.add("fogo")
+    }
+   
+    const divElemento = document.getElementById("conteudo-pokemon")
+
+    divCardElemento.appendChild(imagemElemento)
+    divCardElemento.appendChild(textoElemento)
+
+    divElemento.appendChild(divCardElemento)
+
+}
